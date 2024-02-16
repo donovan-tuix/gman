fb6b108841e510f65ab22d9856a0f3e7728ed951
donovan-tuix <donovan.burbano@tuix.ch>
2024-02-16 09:34:13 -0500

    feat: add docs

--- config_manager/config_manager.go
+++ config_manager/config_manager.go
@@ -11,6 +11,7 @@ import (
 	"github.com/donovan-tuix/gman/config_paths"
 )
 
+// ConfigManager is a struct that holds the paths to the SSH config file and the SSH key file, as well as the file descriptor for the SSH config file.
 type ConfigManager struct {
 	Paths config_paths.ConfigPaths
 	File  *os.File
@@ -18,6 +19,7 @@ type ConfigManager struct {
 
 var identityFileRegex = regexp.MustCompile(`^\s*IdentityFile\s+`)
 
+// NewConfigManager creates a new ConfigManager struct and returns a pointer to it. It takes a newAccountName string as an argument, which is the name of the account that the user wants to use to authenticate with GitHub.
 func NewConfigManager(newAccountName string) (*ConfigManager, error) {
 	paths, err := config_paths.NewConfigPaths(newAccountName)
 	if err != nil {
@@ -35,6 +37,7 @@ func NewConfigManager(newAccountName string) (*ConfigManager, error) {
 	}, nil
 }
 
+// Update reads the SSH config file and replaces the IdentityFile line with the path to the SSH key file. It returns a slice of strings, which is the new SSH config file, and an error, if any.
 func (cm *ConfigManager) Update() ([]string, error) {
 	var newConfig []string
 	scanner := bufio.NewScanner(cm.File)
@@ -65,12 +68,14 @@ func (cm *ConfigManager) Update() ([]string, error) {
 	return newConfig, nil
 }
 
+// WriteToFile writes the new SSH config file to the SSH config file. It takes a slice of strings as an argument, which is the new SSH config file. It returns an error, if any.
 func (cm *ConfigManager) WriteToFile(lines []string) error {
 	newConfigStr := strings.Join(lines, "\n")
 	_, err := io.WriteString(cm.File, newConfigStr+"\n")
 	return err
 }
 
+// Close closes the file descriptor for the SSH config file.
 func (cm *ConfigManager) Close() {
 	cm.File.Close()
 }
--- config_paths/config_paths.go
+++ config_paths/config_paths.go
@@ -5,11 +5,13 @@ import (
 	"path/filepath"
 )
 
+// ConfigPaths is a struct that holds the paths to the SSH config file and the SSH key file.
 type ConfigPaths struct {
 	ConfigPath string
 	KeyPath    string
 }
 
+// NewConfigPaths creates a new ConfigPaths struct and returns a pointer to it. It takes a newAccountName string as an argument, which is the name of the account that the user wants to use to authenticate with GitHub.
 func NewConfigPaths(newAccountName string) (*ConfigPaths, error) {
 	homeDir, err := user.Current()
 	if err != nil {
